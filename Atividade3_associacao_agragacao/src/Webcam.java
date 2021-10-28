65a9d370a1854e0e6663ba4b38086c68531339f4
Yasmin Castro <yasminsdcastro@gmail.com>
2021-10-27 21:00:49 -0300

    refactor: correção

---
+++ Atividade3_associacao_agragacao/src/Webcam.java
@@ -0,0 +1,27 @@
+
+public class Webcam {
+	
+	private String marca;
+	private String MP;
+	
+	public Webcam(String marca, String MP) {
+		this.marca = marca;
+		this.MP = MP;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getMP() {
+		return MP;
+	}
+	public void setMP(String mP) {
+		MP = mP;
+	}
+	
+	
+	
+}
